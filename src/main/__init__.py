f1fd474fb203282e21625df691af918d0092a226
steven <751836594@qq.com>
2018-01-03 15:08:51 +0800

    init code

---
+++ src/main/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: 3.6
+@author: steven
+@license: Apache Licence 
+@contact: 751836594@qq.com
+@site: 
+@software: PyCharm
+@file: __init__.py
+@time: 2018/1/3 下午2:46
+"""
+
+
+def func():
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ src/run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: 3.6
+@author: steven
+@license: Apache Licence 
+@contact: 751836594@qq.com
+@site: 
+@software: PyCharm
+@file: run.py
+@time: 2018/1/3 下午2:46
+"""
+from pypinyin import lazy_pinyin
+
+
+def func():
+    pass
+
+
+if __name__ == '__main__':
+    s = lazy_pinyin('你hai好吗')
